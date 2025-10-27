9fe15276455dc54fbc1f04d2200c5d09920c047a
Hyungseok Kim <sw.sec.research.lab@gmail.com>
2025-10-27 22:01:48 +0900

    'Fix'

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include "bird.h"
 
+    Duck::Duck(): Bird("Duck"){ cry(); };
+    void Duck::cry() {
+        std::cout << "Quack!" << std::endl;
+    }
+    Duck::~Duck() { cry();}
+
+    Eagle::Eagle(): Bird("Eagle"){ cry(); };
+    void Eagle::swim() {
+        std::cout << "This bird can't swim." << std::endl;
+    }
+    void Eagle::cry() {
+        std::cout << "Screech!" << std::endl;
+    }
+    Eagle::~Eagle() { cry();}
+
+    Penguin::Penguin(): Bird("Penguin"){ cry(); };
+    void Penguin::fly() {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+    void Penguin::cry() {
+        std::cout << "Squawk!" << std::endl;
+    }
+    Penguin::~Penguin() { cry();}
+
+
